276f3aefa9f344361e01fd579b0bbeb6ca5f8008
Pratham <prathamsharmafaangm@gmail.com>
2022-05-22 02:38:21 +0530

    Responsive navbar with JS added

--- PIZZA Website/js/script.js
+++ PIZZA Website/js/script.js
@@ -2,8 +2,24 @@
 
 
 // Nav open close
+const body = document.querySelector('body'),
+      navMenu = body.querySelector('.menu-content'),
+      navOpenBtn = body.querySelector('.navOpen-btn'),
+      navCloseBtn = navMenu.querySelector('.navClose-btn');
+      
+if(navMenu && navOpenBtn){
+    navOpenBtn.addEventListener("click", () =>{
+        navMenu.classList.add("open");
+        body.style.overflowY = "hidden";
+    })
+}
+if(navMenu && navOpenBtn){
+    navCloseBtn.addEventListener("click", () =>{
+        navMenu.classList.remove("open");
+        body.style.overflowY = "hidden";
 
-
+    })
+}
 // Change header bg color
 
 
