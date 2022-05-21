013514a06187cfadf49b936ce494aed67e037361
Pratham <prathamsharmafaangm@gmail.com>
2022-05-22 04:07:13 +0530

    Homepage and transitions added

--- PIZZA Website/js/script.js
+++ PIZZA Website/js/script.js
@@ -1,5 +1,17 @@
 // Swiper js
-
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    // grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 // Nav open close
 const body = document.querySelector('body'),
